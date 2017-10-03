f615713bb6bf7f443e1bead63705f0b9db4c7387
Eirik Isene <iseneirik@gmail.com>
2017-10-03 12:32:11 +0200

    test: add failing test
    Add folder "PackageTemplate" to CSharpCompilerSyntaxTestProject, add
    class "PackageTemplateSyntaxTest.cs" to this folder and ensure that it
    runs along with the other tests, successful! This is a good starting
    point.
    
    TODO: Create tests that match the PackageTemplate Syntax, start simple
    and after making the tests go green, carry on to more complex versions

---
+++ src/Compilers/CSharp/Test/Syntax/PackageTemplate/PackageTemplateSyntaxTest.cs
@@ -0,0 +1,22 @@
+﻿using Roslyn.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.CSharp.UnitTests.PackageTemplate
+{
+    public class PackageTemplateSyntaxTest : TestBase
+    {
+        [Fact]
+        public void WarmUp()
+        {
+            var text = @"
+class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        Console.WriteLine(""Hello, world!"");
+    }
+}";
+            Assert.Empty(text);
+        }
+    }
+}
