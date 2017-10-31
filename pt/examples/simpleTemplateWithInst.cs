8ba645ddbf32039f446927afdaf65d0a98d2a96e
Eirik Isene <iseneirik@gmail.com>
2017-10-31 11:13:02 +0100

    docs: update example of PackageTemplate usage

--- pt/examples/simpleTemplateWithInst.cs
+++ pt/examples/simpleTemplateWithInst.cs
@@ -1,5 +1,4 @@
-
-
+/* WITH ONLY ONE TEMPLATE */
 namespace N
 {
 	template T 
@@ -36,4 +35,36 @@ namespace N
 		};
 	}
 	
+}
+
+/* WITH MULTIPLE TEMPLATES */
+namespace N 
+{
+	template T1
+	{
+		class A
+		{
+			int i;
+			int j;
+		}
+	}
+
+	template T2
+	{
+		class A
+		{
+			int k;
+		}
+	}
+
+	inst T1, T2
+	{
+		T1.A ~> AA (i ~> ii, j ~> jj);
+		T2.A ~> AA (k ~> kk);
+
+		AA
+		{
+			int ll;
+		}
+	}
 }
