490d10de3b218ff38ecc6eab86afde4f51f39187
Eirik Isene <iseneirik@gmail.com>
2018-02-26 10:06:20 +0100

    feature: add MergedTemplateDeclaration
    Both Types and Namespaces have a "Merged" version of their declaration,
    the reason for this is that they can be split over multiple files, eg.
    partial classes, or namespaces that are declared in multiple files. This
    is not the case for Templates (at least not now), so the
    MergedTemplateDeclaration class is simply just a wrapper around a
    TemplateDeclaration, the reason for this is that we need a subclass of
    MergedNamespaceOrTypeDeclaration to conform to the parameters in certain
    methods in the compiler.

---
+++ src/Compilers/CSharp/Portable/Declarations/MergedTemplateDeclaration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace Microsoft.CodeAnalysis.CSharp
+{
+    class MergedTemplateDeclaration : MergedNamespaceOrTypeDeclaration
+    {
+        private readonly TemplateDeclaration _declaration;
+
+        public MergedTemplateDeclaration(TemplateDeclaration declaration)
+            : base(declaration.Name)
+        {
+            _declaration = declaration;
+        }
+
+        public TemplateDeclaration Declaration => _declaration;
+        public override DeclarationKind Kind => DeclarationKind.Template;
+
+        protected override ImmutableArray<Declaration> GetDeclarationChildren()
+        {
+            return _declaration.Children.Cast<SingleNamespaceOrTypeDeclaration, Declaration>();
+        }
+    }
+}
